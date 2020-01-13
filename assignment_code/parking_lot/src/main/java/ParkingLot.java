5408c4379f333fd0a9bb2a1c3bcf53b3e12f84eb
Christian <Christian.s.mark@hotmail.com>
2020-01-13 22:19:16 +0100

    Added xml and server<->client communication

--- assignment_code/parking_lot/src/main/java/ParkingLot.java
+++ assignment_code/parking_lot/src/main/java/ParkingLot.java
@@ -1,16 +1,24 @@
+import org.eclipse.leshan.LwM2m;
+import org.eclipse.leshan.core.model.ObjectLoader;
+import org.eclipse.leshan.core.model.ObjectModel;
 import org.eclipse.leshan.server.californium.LeshanServer;
 import org.eclipse.leshan.server.californium.LeshanServerBuilder;
+import org.eclipse.leshan.server.model.LwM2mModelProvider;
+import org.eclipse.leshan.server.model.VersionedModelProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import servlet.EventServlet;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 import java.net.InetAddress;
-
+import java.util.List;
 
 public class ParkingLot {
 
     private static final Logger LOG = LoggerFactory.getLogger(ParkingLot.class);
+
+    private final static String[] modelPaths = new String[] { "32702.xml"};
 
     private static LeshanServerBuilder builder = new LeshanServerBuilder();
     private static LeshanServer server = builder.build();
@@ -18,11 +26,20 @@ public class ParkingLot {
     public static void main(String[] args) throws Exception {
         LOG.info("Launching server...");
 
+        // Define model provider
+        List<ObjectModel> models = ObjectLoader.loadDefault();
+        models.addAll(ObjectLoader.loadDdfResources("/models/", modelPaths));
+        LwM2mModelProvider modelProvider = new VersionedModelProvider(models);
+        builder.setObjectModelProvider(modelProvider);
+
+        // Create Servlet
+        EventServlet eventServlet = new EventServlet(server, 1337);
+
         // Create a JmDNS instance
         final JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost().getCanonicalHostName());
 
         // Publish Leshan CoAP Service
-        ServiceInfo coapServiceInfo = ServiceInfo.create("_parkingserver._udp.", "P1", 5683, "");
+        ServiceInfo coapServiceInfo = ServiceInfo.create("_parkingserver._udp.", "P1", LwM2m.DEFAULT_COAP_PORT, "");
         jmdns.registerService(coapServiceInfo);
 
         server.start();
--- assignment_code/parking_spot/src/main/java/ParkingSpot.java
+++ assignment_code/parking_spot/src/main/java/ParkingSpot.java
@@ -1,30 +1,55 @@
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.leshan.LwM2m;
 import org.eclipse.leshan.client.californium.LeshanClient;
 import org.eclipse.leshan.client.californium.LeshanClientBuilder;
+import org.eclipse.leshan.client.object.Server;
+import org.eclipse.leshan.client.resource.LwM2mObjectEnabler;
+import org.eclipse.leshan.client.resource.ObjectsInitializer;
+import org.eclipse.leshan.core.model.ObjectLoader;
+import org.eclipse.leshan.core.model.ObjectModel;
+import org.eclipse.leshan.core.model.StaticModel;
+import org.eclipse.leshan.core.request.BindingMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+import java.io.File;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.List;
+
+import static org.eclipse.leshan.LwM2mId.SECURITY;
+import static org.eclipse.leshan.LwM2mId.SERVER;
+import static org.eclipse.leshan.client.object.Security.noSec;
+import static org.eclipse.leshan.client.object.Security.noSecBootstap;
 
 
 public class ParkingSpot {
 
     private static final Logger LOG = LoggerFactory.getLogger(ParkingSpot.class);
 
-    static String endpoint = "..." ; // choose an endpoint name
+    static String endpoint = "Megalulz-ParkingSpot" ; // choose an endpoint name
+    private final static String[] modelPaths = new String[]{"32700.xml"};
 
-    private static LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
-    private static LeshanClient client = builder.build();
+    private static final int OBJECT_ID_PARKING_SPOT = 32700;
 
     public static void main(String[] args) throws Exception {
         LOG.info("Launching client...");
 
-        client.start();
+        try {
+            endpoint = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+
+        }
+
+        //get serverURI
+        String serverURI = "coap://localhost:" + LwM2m.DEFAULT_COAP_PORT; //TODO: SHOULD GET FROM JmDNS
+
         // Create a JmDNS instance
         final JmDNS jmdns = JmDNS.create();
-        for (int i = 1; i < 4; i++) {
+        for (int i = 1; i < 2; i++) {
             ServiceInfo p = jmdns.getServiceInfo("_parkingserver._udp.", "P"+i,3000);
             if(p != null){
                 System.out.println(p.toString());
@@ -32,6 +57,67 @@ public class ParkingSpot {
             }
         }
 
+        // get local address
+        String localAddress = "localhost";
+        int localPort = 7331;
+
+        try {
+            createAndStartClient(endpoint, localAddress, localPort, serverURI);
+        } catch (Exception e) {
+            System.err.println("Unable to create and start client ...");
+            e.printStackTrace();
+            return;
+        }
+
         System.out.println("Ended");
+
+    }
+
+    private static void createAndStartClient(String endpoint, String localAddress, int localPort, String serverURI) {
+
+        // Initialize model
+        List<ObjectModel> models = ObjectLoader.loadDefault();
+        models.addAll(ObjectLoader.loadDdfResources("/models", modelPaths));
+
+        // Initialize object list
+        ObjectsInitializer initializer = new ObjectsInitializer(new StaticModel(models));
+        initializer.setInstancesForObject(OBJECT_ID_PARKING_SPOT, new ParkingSpot_model());
+        initializer.setInstancesForObject(SECURITY, noSecBootstap(serverURI));
+        initializer.setClassForObject(SERVER, Server.class);
+        initializer.setInstancesForObject(SECURITY, noSec(serverURI, 123));
+        initializer.setInstancesForObject(SERVER, new Server(123, 30, BindingMode.U, false));
+
+        List<LwM2mObjectEnabler> enablers = initializer.createAll();
+
+        // Create CoAP Config
+        NetworkConfig coapConfig;
+        File configFile = new File(NetworkConfig.DEFAULT_FILE_NAME);
+        if (configFile.isFile()) {
+            coapConfig = new NetworkConfig();
+            coapConfig.load(configFile);
+        } else {
+            coapConfig = LeshanClientBuilder.createDefaultNetworkConfig();
+            coapConfig.store(configFile);
+        }
+
+        // Create client
+        LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
+        builder.setLocalAddress(localAddress, localPort);
+        builder.setObjects(enablers);
+        builder.setCoapConfig(coapConfig);
+        final LeshanClient client = builder.build();
+
+
+        client.start();
+        System.out.println("ended");
+
+        // De-register on shutdown and stop client.
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                client.destroy(true); // send de-registration request before destroy
+            }
+        });
+
     }
 }
