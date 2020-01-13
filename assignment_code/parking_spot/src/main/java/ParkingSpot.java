4ac62388137f031332113b47df02d451e63ad071
Tomas Tombakas <tomas.tombakas@sendcloud.com>
2020-01-13 20:50:48 +0100

    Semi functioning mdns resolver in parking spot

--- assignment_code/parking_spot/src/main/java/ParkingSpot.java
+++ assignment_code/parking_spot/src/main/java/ParkingSpot.java
@@ -3,6 +3,15 @@ import org.eclipse.leshan.client.californium.LeshanClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+
 
 public class ParkingSpot {
 
@@ -13,7 +22,41 @@ public class ParkingSpot {
     private static LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
     private static LeshanClient client = builder.build();
 
+    private static class SampleListener implements ServiceListener {
+        @Override
+        public void serviceAdded(ServiceEvent event) {
+            System.out.println("Service added: " + event.getInfo());
+        }
+
+        @Override
+        public void serviceRemoved(ServiceEvent event) {
+            System.out.println("Service removed: " + event.getInfo());
+        }
+
+        @Override
+        public void serviceResolved(ServiceEvent event) {
+            System.out.println("Service resolved: " + event.getInfo());
+        }
+    }
+
     public static void main(String[] args) {
+        try {
+            // Create a JmDNS instance
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            // Add a service listener
+            jmdns.addServiceListener("_parkingserver._udp.local.", new SampleListener());
+
+            // Wait a bit
+            Thread.sleep(30000);
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println("Interrupt receiverd");
+        }
+
         LOG.info("Launching client...");
         client.start();
     }
