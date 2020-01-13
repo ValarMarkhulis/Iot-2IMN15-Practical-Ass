f9fe20355771c7b5b6446fc0c3b2ebe720c6fbce
Christian <Christian.s.mark@hotmail.com>
2020-01-13 19:49:31 +0100

    Parking spot discovers parking lots

--- assignment_code/parking_spot/src/main/java/ParkingSpot.java
+++ assignment_code/parking_spot/src/main/java/ParkingSpot.java
@@ -3,6 +3,11 @@ import org.eclipse.leshan.client.californium.LeshanClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import java.net.InetAddress;
+import java.util.Arrays;
+
 
 public class ParkingSpot {
 
@@ -13,8 +18,20 @@ public class ParkingSpot {
     private static LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
     private static LeshanClient client = builder.build();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         LOG.info("Launching client...");
+
         client.start();
+        // Create a JmDNS instance
+        final JmDNS jmdns = JmDNS.create();
+        for (int i = 1; i < 4; i++) {
+            ServiceInfo p = jmdns.getServiceInfo("_parkingserver._udp.", "P"+i,3000);
+            if(p != null){
+                System.out.println(p.toString());
+                System.out.println("p"+1+" is ="+ Arrays.toString(p.getHostAddresses()));
+            }
+        }
+
+        System.out.println("Ended");
     }
 }
