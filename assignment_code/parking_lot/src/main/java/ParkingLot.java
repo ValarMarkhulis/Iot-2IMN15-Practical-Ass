869d8129cee6fe355f8a1d46738057c56249583f
Christian <Christian.s.mark@hotmail.com>
2020-01-13 18:56:11 +0100

    ParkingLot Sending out mDNS Broadcast

--- assignment_code/parking_lot/src/main/java/ParkingLot.java
+++ assignment_code/parking_lot/src/main/java/ParkingLot.java
@@ -3,6 +3,10 @@ import org.eclipse.leshan.server.californium.LeshanServerBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import java.net.InetAddress;
+
 
 public class ParkingLot {
 
@@ -11,8 +15,29 @@ public class ParkingLot {
     private static LeshanServerBuilder builder = new LeshanServerBuilder();
     private static LeshanServer server = builder.build();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         LOG.info("Launching server...");
+        // Register a service to DNS-SD
+
+
+        // Create a JmDNS instance
+        final JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+        // Publish Leshan CoAP Service
+        ServiceInfo coapServiceInfo = ServiceInfo.create("_parkingserver._udp.", "P1", 5683, "");
+        jmdns.registerService(coapServiceInfo);
+
         server.start();
+
+
+        // De-register on shutdown and stop client.
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                server.destroy(); // send de-registration request before destroy
+                jmdns.unregisterAllServices();
+            }
+        });
+
     }
 }
