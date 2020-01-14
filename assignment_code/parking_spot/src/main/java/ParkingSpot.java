77228d8ff33420970f8862a763309b130f6b7ed9
Tomas Tombakas <tombakas@gmail.com>
2020-01-14 23:08:15 +0100

    Improved service discoverer

--- assignment_code/parking_spot/src/main/java/ParkingSpot.java
+++ assignment_code/parking_spot/src/main/java/ParkingSpot.java
@@ -7,25 +7,30 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import java.net.InetAddress;
 
+import java.util.ArrayList;
+
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceInfo;
 import javax.jmdns.ServiceListener;
+import javax.jmdns.impl.ServiceEventImpl;
 
 
 public class ParkingSpot {
 
     private static final Logger LOG = LoggerFactory.getLogger(ParkingSpot.class);
+    private static ArrayList parkingServerNames = new ArrayList<String>();
 
-    static String endpoint = "..." ; // choose an endpoint name
+    // static String endpoint = "..." ; // choose an endpoint name
 
-    private static LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
-    private static LeshanClient client = builder.build();
+    // private static LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
+    // private static LeshanClient client = builder.build();
 
     private static class SampleListener implements ServiceListener {
         @Override
         public void serviceAdded(ServiceEvent event) {
             System.out.println("Service added: " + event.getInfo());
+            parkingServerNames.add(event.getName());
         }
 
         @Override
@@ -48,7 +53,11 @@ public class ParkingSpot {
             jmdns.addServiceListener("_parkingserver._udp.local.", new SampleListener());
 
             // Wait a bit
-            Thread.sleep(30000);
+            Thread.sleep(3000);
+
+            for (int i = 0; i < parkingServerNames.size(); i++) {
+               System.out.println(parkingServerNames.get(i));
+            }
         } catch (UnknownHostException e) {
             System.out.println(e.getMessage());
         } catch (IOException e) {
@@ -57,7 +66,7 @@ public class ParkingSpot {
             System.out.println("Interrupt receiverd");
         }
 
-        LOG.info("Launching client...");
-        client.start();
+        // LOG.info("Launching client...");
+        // client.start();
     }
 }
