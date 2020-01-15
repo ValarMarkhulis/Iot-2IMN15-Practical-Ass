dd26fc8985ba1ed9554eb155cea340ef4ff2c2c9
Tomas Tombakas <tomas.tombakas@sendcloud.com>
2020-01-15 23:00:54 +0100

    Update base logic python

--- python_code/led_matrix_base.py
+++ python_code/led_matrix_base.py
@@ -36,13 +36,13 @@ def get_pixel_params():
         sys.exit()
     X = sys.argv[1]
     Y = sys.argv[2]
-    color = sys.argv[2]
+    color = sys.argv[3]
 
     if color not in "ROG":
         print("Invalid color provided")
         sys.exit(1)
 
-    if (X > 7 or Y > 7):
+    if (int(X) > 7 or int(Y) > 7):
         print("Bad coordinates provided: x={}, y={}".format(X, Y))
         sys.exit(1)
 
